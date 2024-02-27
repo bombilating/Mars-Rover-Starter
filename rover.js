6c9344aca85e1423d5f14c5e43ec82c9390871ab
Jeremy <thisismejeremy@gmail.com>
2024-02-27 02:05:38 -0500

    graded assignment 3

--- rover.js
+++ rover.js
@@ -1,4 +1,4 @@
-class Rover {
+class Rover    {
    // Write code here!
    constructor(position) {
       this.position = position;
@@ -6,9 +6,31 @@ class Rover {
       this.generatorWatts = 110;
    }
 
-   receiveMessage(message) {
-      
+   receiveMessage(message) { 
+      let messageName = message.name
+      let commandResults = []
+      for(let i = 0; i < message.commands.length; i++) {
+         if (message.commands[i]["commandType"] === "STATUS_CHECK") {
+            commandResults.push({completed: true, roverStatus: {mode: this.mode, generatorWatts: this.generatorWatts, position: this.position}})
+         } else {
+            if (message.commands[i]["commandType"] === "MODE_CHANGE") {
+               this.mode = message.commands[i]["value"]
+               commandResults.push({completed: true})
+            } else {
+               if (message.commands[i]["commandType"] === "MOVE") {
+                  if(this.mode === "NORMAL") {
+                     this.position = message.commands[i]["value"]
+                     commandResults.push({completed: true})
+                  } else {
+                     commandResults.push({completed: false})
+                     }
+               } else {
+               commandResults.push({completed: false})
+               }
+          }
+         }
+      }
+      return {message: messageName, results: commandResults}
    }
 }
-
 module.exports = Rover;
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -17,9 +17,53 @@ describe("Rover class", function() {
   });
 
   it("response returned by receiveMessage contains the name of the message", function() {
-    let testCommands = [new Command('1', '2'), new Command('3')]
+    let testCommands = [new Command('1', '2'), new Command('MODE_CHANGE')]
     let testMessage = new Message('Hello world', testCommands);
     let testRover = new Rover(10);
-    expect(testRover.receiveMessage(testMessage)[]).toBe();
+    expect(testRover.receiveMessage(testMessage)["message"]).toBe('Hello world');
   })
-});
+
+  it("response returned by receiveMessage includes two results if two commands are sent in the message", function() {
+    let testCommands = [new Command("STATUS_CHECK"), new Command('3')]
+    let testMessage = new Message('Hello world', testCommands);
+    let testRover = new Rover(10);
+    expect(testRover.receiveMessage(testMessage)["results"].length).toBe(2)
+  });
+
+  it("responds correctly to the status check command", function() {
+    let testCommands = [new Command("STATUS_CHECK")]
+    let testMessage = new Message('Hello world', testCommands);
+    let testRover = new Rover(87382098);
+    let testObj = testRover.receiveMessage(testMessage)["results"][0]["roverStatus"]
+    expect(testObj["mode"]).toBe("NORMAL");
+    expect(testObj["generatorWatts"]).toBe(110);
+    expect(testObj["position"]).toBe(87382098);
+  })
+
+  it("responds correctly to the mode change command", function() {
+    let testCommands = [new Command("MODE_CHANGE", "LOW_POWER")];
+    let testMessage = new Message('Hello world', testCommands);
+    let testRover = new Rover(87382098);
+    let testObj = testRover.receiveMessage(testMessage)["results"][0]
+    expect(testObj["completed"]).toBe(true)
+    expect(testRover.mode).toBe("LOW_POWER");
+  })
+
+  it("responds with a false completed value when attempting to move in LOW_POWER mode", function() {
+    let testCommands = [new Command("MODE_CHANGE", "LOW_POWER"), new Command("MOVE", 5)];
+    let testMessage = new Message('Hello world', testCommands);
+    let testRover = new Rover(87382098);
+    let testObj = testRover.receiveMessage(testMessage)["results"]
+    expect(testObj[1]["completed"]).toBe(false)
+    expect(testRover.position).toBe(87382098);
+  });
+
+  it("responds with the position for the move command", function() {
+    let testCommands = [new Command("MOVE", 5)];
+    let testMessage = new Message('Hello world', testCommands);
+    let testRover = new Rover(87382098);
+    let testObj = testRover.receiveMessage(testMessage)["results"]
+    expect(testObj[0]["completed"]).toBe(true)
+    expect(testRover.position).toBe(5);
+  })
+})
